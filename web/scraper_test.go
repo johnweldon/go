bce79288e0d6de192c50fbfe4d4a31b6a56de9ac
John Weldon <johnweldon4@gmail.com>
2014-04-23 15:51:34 -0700

    test url scraping

--- web/scraper_test.go
+++ web/scraper_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 )
 
-var testResponse string = "<http><head></head><body><div>Raw Text</div><p class='test3'>Paragraph</p><span>span</span></body></http>"
+var testResponse string = "<http><head></head><body><div>Raw Text</div><p class='test3'>Paragraph</p><span>span</span><a href='fakeurl' /><a href='fakeurl2'></a></body></http>"
 
 type testServerHandler struct{}
 
@@ -68,6 +68,49 @@ func TestCustomScrape(t *testing.T) {
 	}
 }
 
+func TestGetAttr(t *testing.T) {
+	l, e := startServer()
+	if e != nil {
+		t.Error(e)
+	}
+	defer l.Close()
+
+	scraper := NewWebScraper()
+	scraper.ScrapePredicates = []ScrapePredicate{HasAttr("href")}
+	scraper.ScrapeAction = AttrValue("href")
+	addr := "http://" + l.Addr().String() + "/blah/blah"
+	t.Logf("Addr '%s'\n", addr)
+	s, e := scraper.Scrape(addr)
+
+	if e != nil {
+		t.Error(e)
+	}
+	if s == nil {
+		t.Error("Returned nothing")
+	}
+	if s.Raw() == "" {
+		t.Error("Raw result is empty")
+	}
+	expected, got := []interface{}{"fakeurl", "fakeurl2"}, s.Matches()
+	if len(got) != 2 {
+		t.Error("Found wrong number of matches", len(got), got)
+	}
+	if !same(expected, got) {
+		t.Error("Expected: ", expected, ", Got: ", got)
+	}
+}
+
+func same(expected, got []interface{}) bool {
+	if len(expected) != len(got) {
+		return false
+	}
+	for ix, v := range expected {
+		if v != got[ix] {
+			return false
+		}
+	}
+	return true
+}
 func startServer() (net.Listener, error) {
 	l, e := net.Listen("tcp", "127.0.0.1:0")
 	if e != nil {
