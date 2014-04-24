99fcaef368fbfef2a8e14dc4a5be8e9875b1a20c
John Weldon <johnweldon4@gmail.com>
2014-04-23 17:00:05 -0700

    list links on a page

---
+++ cmd/get-links/get-links.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/johnweldon/go/web"
+)
+
+type Links struct {
+	Root  string
+	Links []string
+}
+
+func (l Links) String() string {
+	r := fmt.Sprintf("> %s\n", l.Root)
+	for _, v := range l.Links {
+		r = fmt.Sprintf("%s- %s\n", r, v)
+	}
+	return r
+}
+
+type S struct {
+	web.WebScrape
+}
+
+func NewS() *S {
+	scraper := &S{}
+	scraper.ScrapePredicates = []web.ScrapePredicate{web.HasAttr("href")}
+	scraper.ScrapeAction = web.AttrValue("href")
+	return scraper
+}
+
+func (s *S) Crawl(url string) (Links, error) {
+	links := Links{url, []string{}}
+	r, e := s.Scrape(url)
+	if e != nil {
+		return Links{}, e
+	}
+	for _, v := range r.Matches() {
+		links.Links = append(links.Links, v.(string))
+	}
+	return links, nil
+}
+
+func init() {
+}
+
+func main() {
+	flag.Parse()
+	s := NewS()
+	for _, root := range flag.Args() {
+		r, e := s.Crawl(root)
+		if e != nil {
+			fmt.Fprintf(os.Stderr, "Error crawling '%s': '%v'\n", e)
+		}
+		fmt.Fprintf(os.Stdout, "%s\n", r)
+	}
+}
