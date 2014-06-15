5657cc2798bc4770bb10470993931788069903db
John Weldon <johnweldon4@gmail.com>
2014-06-14 23:27:16 -0700

    concurrent

--- cmd/get-irclogs/main.go
+++ cmd/get-irclogs/main.go
@@ -3,88 +3,114 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"net/http"
+	"net/url"
 	"os"
-	"strings"
-
-	"code.google.com/p/go.net/html"
+	"sync"
+	"time"
 )
 
+var wg = []sync.WaitGroup{sync.WaitGroup{}, sync.WaitGroup{}}
+var urls = make(chan downloadData, 4)
+
+type downloadData struct {
+	URL  string
+	File string
+}
+
+type generateArgs struct {
+	BaseURL string
+	Target  string
+	Begin   time.Time
+}
+
+// Main is a testable delegate of main
 func Main(args []string) {
-	baseurl := "http://irclogs.ubuntu.com"
-	urls := []string{baseurl}
-	target := "#juju-dev.txt"
-
-	client := http.Client{}
-	for {
-		if len(urls) == 0 {
-			break
-		}
-		cur := urls[0]
-		urls = urls[1:]
-		prefix := strings.Replace(cur[len(baseurl):], "/", "-", 0)
-
-		fmt.Fprintf(os.Stderr, "checking %s\n", cur)
-		response, err := client.Get(cur)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "problem reading '%s'; %q\n", cur, err)
-			continue
-		}
-		matches, links := find(response, baseurl, target)
-		urls = append(urls, links...)
-		for _, match := range matches {
-			saveMatch(&client, prefix, target, match)
-		}
+	begin, _ := time.Parse("20060102", "20040101")
+	genArgs := generateArgs{
+		BaseURL: "http://irclogs.ubuntu.com",
+		Target:  "#juju-dev.txt",
+		Begin:   begin,
 	}
-
+	wg[0].Add(2)
+	go generateDates(genArgs)
+	go consumeURLs()
+	wg[0].Wait()
 }
 
-func saveMatch(c *http.Client, prefix, target, match string) {
-	fmt.Fprintf(os.Stdout, "MATCH: %s_%s\n", prefix, target[1:])
-}
+func generateDates(args generateArgs) {
+	defer wg[0].Done()
 
-func find(r *http.Response, baseurl, target string) (matches, links []string) {
-	if r.StatusCode == http.StatusOK {
-		base := r.Request.URL.String()
-		baseUrl, err := r.Location()
-		if err == nil {
-			base = baseUrl.String()
-		}
-		body, err := html.Parse(r.Body)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "html parse error %q\n", err)
+	date := args.Begin
+	now := time.Now()
+	for {
+		if now.Before(date) {
+			close(urls)
 			return
 		}
+		urls <- downloadData{URL: date.Format(args.BaseURL + "/2006/01/02/" + args.Target), File: date.Format("2006-01-02-" + args.Target[1:])}
+		date = date.AddDate(0, 0, 1)
+		select {
+		case <-time.After(500 * time.Millisecond):
+			continue
+		}
+	}
+}
 
-		var findFn func(*html.Node)
-		findFn = func(n *html.Node) {
-			for _, attr := range n.Attr {
-				if attr.Key == "href" {
-					if strings.HasPrefix(attr.Val, "?") {
-						continue
-					}
-					if strings.HasPrefix(attr.Val, "#") {
-						continue
-					}
-					if strings.HasPrefix(attr.Val, "http") && !strings.HasPrefix(attr.Val, baseurl) {
-						continue
-					}
-					link := base + "/" + attr.Val
-					if strings.HasSuffix(link, target) {
-						matches = append(matches, link)
-					} else {
-						links = append(links, link)
-					}
-				}
-			}
-			for c := n.FirstChild; c != nil; c = c.NextSibling {
-				findFn(c)
+func consumeURLs() {
+	defer wg[0].Done()
+
+	for {
+		select {
+		case u, ok := <-urls:
+			if ok {
+				wg[1].Add(1)
+				go downloadURL(u)
+			} else {
+				wg[1].Wait()
+				return
 			}
 		}
-
-		findFn(body)
 	}
-	return
+}
+
+func downloadURL(dd downloadData) {
+	defer wg[1].Done()
+
+	u, err := url.Parse(url.QueryEscape(dd.URL))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "can't parse %s: %q\n", dd.URL, err)
+		return
+	}
+	response, err := http.Get(u.String())
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "can't get %s: %q\n", u, err)
+		return
+	}
+	defer response.Body.Close()
+
+	switch response.StatusCode {
+	case http.StatusOK:
+		fmt.Fprintf(os.Stdout, "Found: %s; saving to %s\n", u, dd.File)
+
+		out, err := os.Create(dd.File)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "can't create output file %s; %q\n", dd.File, err)
+			return
+		}
+		defer out.Close()
+
+		_, err = io.Copy(out, response.Body)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "can't download file %s; %q\n", dd.File, err)
+			return
+		}
+	case http.StatusNotFound:
+		return
+	default:
+		fmt.Fprintf(os.Stdout, "Unexpected response %+v\n", response.Status)
+	}
 }
 
 func main() {
