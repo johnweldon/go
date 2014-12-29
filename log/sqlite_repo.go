dea91de5acde54c858b4670bafe546255a8c018e
John Weldon <johnweldon4@gmail.com>
2014-12-29 14:13:08 -0800

    add sqlite db persistence

---
+++ log/sqlite_repo.go
@@ -0,0 +1,227 @@
+package log
+
+import (
+	"database/sql"
+	"log"
+	"strings"
+	"time"
+
+	_ "github.com/mattn/go-sqlite3"
+
+	"github.com/johnweldon/go/util"
+)
+
+type RelDB struct {
+	dbpath  string
+	ensured bool
+	db      *sql.DB
+}
+
+func NewRelDB(path string) *RelDB {
+	return &RelDB{dbpath: path, ensured: false, db: nil}
+}
+
+func (d *RelDB) Open() func() {
+	if d.db != nil {
+		err := d.db.Ping()
+		if err == nil {
+			return func() {}
+		}
+		log.Print(err)
+		d.db = nil
+	}
+	db, err := sql.Open("sqlite3", d.dbpath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	d.db = db
+	if !d.ensured {
+		err := ensureDb(d.db)
+		if err != nil {
+			log.Fatal(err)
+		}
+		d.ensured = true
+	}
+	return func() {
+		d.db.Close()
+		d.db = nil
+	}
+}
+
+func (d *RelDB) GetRecords() []TimeRecord {
+	defer d.Open()()
+
+	result := []TimeRecord{}
+
+	rows, err := d.db.Query(sqlSelectRecords)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id string
+		var begin string
+		var duration int64
+		var durationstring string
+		var project string
+		var notes string
+		var tagslist string
+
+		err := rows.Scan(&id, &begin, &duration, &durationstring, &project, &notes, &tagslist)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		btime, err := time.Parse(time.RFC3339, begin)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		tags := strings.Split(tagslist, ",")
+		result = append(result, TimeRecord{
+			Id:             util.UUID(id),
+			Begin:          btime,
+			Duration:       time.Duration(duration),
+			DurationString: durationstring,
+			Project:        project,
+			Notes:          notes,
+			Tags:           tags})
+	}
+
+	return result
+}
+
+func (d *RelDB) SaveRecords(records []TimeRecord) error {
+	defer d.Open()()
+
+	tx, err := d.db.Begin()
+	if err != nil {
+		return err
+	}
+
+	insProj, err := tx.Prepare(sqlInsertProject)
+	if err != nil {
+		return err
+	}
+	defer insProj.Close()
+
+	insRec, err := tx.Prepare(sqlInsertRecord)
+	if err != nil {
+		return err
+	}
+	defer insRec.Close()
+
+	insTag, err := tx.Prepare(sqlInsertTag)
+	if err != nil {
+		return err
+	}
+	defer insTag.Close()
+
+	insRecTag, err := tx.Prepare(sqlInsertRecordTag)
+	if err != nil {
+		return err
+	}
+	defer insRecTag.Close()
+
+	for _, record := range records {
+		_, err = insProj.Exec(record.Project)
+		if err != nil {
+			return err
+		}
+		_, err = insRec.Exec(string(record.Id), record.Begin.Format(time.RFC3339), record.Duration, record.DurationString, record.Project, record.Notes)
+		if err != nil {
+			return err
+		}
+		for _, tag := range record.Tags {
+			_, err = insTag.Exec(tag)
+			if err != nil {
+				return err
+			}
+			_, err = insRecTag.Exec(string(record.Id), tag)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	tx.Commit()
+	return nil
+}
+
+func ensureDb(db *sql.DB) error {
+	for _, stmt := range []string{sqlSetupDb, sqlCreateTags, sqlCreateProjects, sqlCreateRecords, sqlCreateRecordsTags} {
+		_, err := db.Exec(stmt)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+const (
+	sqlSetupDb    string = "PRAGMA foreign_keys = ON"
+	sqlCreateTags string = `
+	    CREATE TABLE IF NOT EXISTS tags (
+	        Name TEXT
+	            CONSTRAINT pk_tag_name PRIMARY KEY
+	            ON CONFLICT ROLLBACK
+	    ) `
+	sqlCreateProjects string = `
+	    CREATE TABLE IF NOT EXISTS projects (
+	        Name TEXT
+	            CONSTRAINT pk_project_name PRIMARY KEY
+	            ON CONFLICT ROLLBACK
+        ) `
+	sqlCreateRecords string = `
+	    CREATE TABLE IF NOT EXISTS records (
+            Id TEXT
+                CONSTRAINT pk_record_id PRIMARY KEY
+                ON CONFLICT ROLLBACK,
+            Begin TEXT,
+            Duration INTEGER ,
+            DurationString TEXT ,
+            Project TEXT
+                CONSTRAINT fk_records_project_name REFERENCES projects ( Name )
+                ON DELETE CASCADE ON UPDATE CASCADE ,
+            Notes TEXT
+        ) `
+	sqlCreateRecordsTags string = `
+	    CREATE TABLE IF NOT EXISTS records_tags (
+            RecordId TEXT
+                CONSTRAINT fk_records_tags_record_id REFERENCES records ( Id )
+                ON DELETE CASCADE ON UPDATE CASCADE ,
+            TagName TEXT
+                CONSTRAINT fk_records_tags_tag_name REFERENCES tags ( Name )
+                ON DELETE CASCADE ON UPDATE CASCADE ,
+            CONSTRAINT pk_records_tags_recordid_tagname PRIMARY KEY ( RecordId, TagName )
+                ON CONFLICT ROLLBACK
+        ) `
+
+	sqlInsertTag     string = "INSERT OR IGNORE INTO tags ( Name ) VALUES ( ? )"
+	sqlInsertProject string = "INSERT OR IGNORE INTO projects ( Name ) VALUES ( ? )"
+	sqlInsertRecord  string = `
+        INSERT OR IGNORE INTO records
+        ( Id, Begin, Duration, DurationString, Project, Notes ) VALUES
+        (  ?,     ?,        ?,              ?,       ?,     ? )
+    `
+	sqlInsertRecordTag string = `
+        INSERT OR IGNORE INTO records_tags
+        ( RecordId, TagName ) VALUES
+        (        ?,       ? )
+    `
+
+	sqlSelectRecords string = `
+	    SELECT
+	        R.Id ,
+	        R.Begin ,
+	        R.Duration ,
+	        R.DurationString ,
+	        R.Project ,
+	        R.Notes ,
+	        group_concat(RT.TagName) AS Tags
+	    FROM records AS R
+	    LEFT OUTER JOIN projects AS P ON R.Project = P.Name
+	    LEFT OUTER JOIN records_tags AS RT ON RT.RecordId = R.Id
+	    GROUP BY R.Id
+    `
+)
---
+++ log/sqlite_repo_test.go
@@ -0,0 +1,52 @@
+package log_test
+
+import (
+	"os"
+	"testing"
+	"time"
+
+	"github.com/johnweldon/go/log"
+)
+
+const dbpath string = "./test.db"
+
+func TestInit(t *testing.T) {
+	os.Remove(dbpath)
+
+	db := log.NewRelDB(dbpath)
+	defer os.Remove(dbpath)
+
+	records := []log.TimeRecord{}
+
+	r1 := log.NewTimeRecord()
+	r1.Duration = time.Duration(15) * time.Minute
+	r1.DurationString = r1.Duration.String()
+	r1.Project = "Project One"
+	r1.Notes = "All my Notes"
+	r1.Tags = append(r1.Tags, "Hello")
+	r1.Tags = append(r1.Tags, "World")
+	records = append(records, r1)
+
+	r2 := log.NewTimeRecord()
+	r2.Duration = time.Duration(3) * time.Hour
+	r2.DurationString = r2.Duration.String()
+	r2.Project = "Project Two"
+	r2.Notes = "Other Notes"
+	r2.Tags = append(r2.Tags, "Goodbye")
+	r2.Tags = append(r2.Tags, "World")
+	records = append(records, r2)
+
+	err := db.SaveRecords(records)
+	if err != nil {
+		t.Error(err)
+	}
+
+	res := db.GetRecords()
+	if len(res) != len(records) {
+		t.Fail()
+	}
+
+	for _, rec := range res {
+		t.Log(rec)
+	}
+}
