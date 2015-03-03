d524769f14c690ab5d799115becbca57d4f47045
John Weldon <johnweldon4@gmail.com>
2015-03-02 19:30:03 -0800

    moved to github.com/johnweldon/cleanpath

--- cmd/cleanpath/cleanpath_test_windows.go
+++
@@ -1,7 +0,0 @@
-package main
-
-var testCase1 map[string]string = map[string]string{
-	"/a/b/c;/a/b/c;/a/b/c/": "/a/b/c",
-	"../a/b;/a/b;../a/b":    "/a/b",
-	"/a;/b;/c;/b;/a/;a":     "/a;/b;/c",
-}
